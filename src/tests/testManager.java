fcae66503fe4e40c715a22c44b304b9ad31aff42
Boin Clément <clem.boin@gmail.com>
2015-05-24 23:40:44 +0200

    Implementation of all tests for Manager

---
+++ src/tests/testManager.java
@@ -0,0 +1,58 @@
+/*
+ * This program is free software: you can redistribute it and/or modify it under the terms of the
+ * GNU General Public License as published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
+ * even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License along with this program. If
+ * not, see <http://www.gnu.org/licenses/>.
+ */
+
+package tests;
+
+import static org.junit.Assert.fail;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import org.junit.Test;
+
+import user.Manager;
+
+/**
+*
+* @author boinc
+*/
+
+public class testManager {
+  
+  @Test
+  public final void testPassword() {
+    Manager m = new Manager("Paul", "Durant");
+    if (!m.getPassword().matches("[a-zA-Z0-9]*")) {
+      fail("generatePassword() not valid");
+    }
+  }
+  
+  @Test
+  public final void testUserName() {
+    Manager m = new Manager("Paul", "Durant");
+    if (!m.getUserName().matches("[a-z]*")) {
+      fail("generateUserName() not valid");
+    }
+  }
+  
+  @Test
+  public final void testEmail() {
+    Manager m = new Manager("Paul", "Durant");
+    Pattern p = Pattern.compile("^[A-Z0-9._%+-]+@[A-Z0-9.-]+\\.[A-Z]{2,4}$");
+    Matcher ma = p.matcher(m.getEmail().toUpperCase());
+    if (!ma.matches()) {
+      fail();
+    }
+  }
+  
+}
