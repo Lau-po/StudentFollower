a4e211d13f664b83c9f2cee67960f4f2ba81c9f9
Boin Clément <clem.boin@gmail.com>
2015-05-24 23:18:57 +0200

    Create file that will contain all constants

---
+++ src/config/Constants.java
@@ -0,0 +1,7 @@
+package config;
+
+public class Constants {
+  public static String EMAIL_SEPARATOR = "@";
+  public static String EMAIL_DN = "univ-lille1.fr";
+  public static String EMAIL_DN_STUDENT = "student.univ-lille1.fr";
+}
