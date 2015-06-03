745993a567a1ae508c6d6bcbd9807d3a9926197d
Clément Boin <clem.boin@gmail.com>
2015-06-03 10:26:06 +0200

    build the login screen

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,17 @@
+import java.io.IOException;
+
+import user.Professor;
+import utils.Util;
+import gui.Login;
+
 public class Main {
-	
+	public static void main(String[] args) {
+		try {
+			Util.findProfessors();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		Login l = new Login();
+	}
 }
